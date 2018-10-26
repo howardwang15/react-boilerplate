13c70aef44fbd8fbb98c695397654418f30b8169
howardwang15 <joy_luck_2000@hotmail.com>
2018-10-26 10:03:02 -0700

    setup boilerplate with babel and webpack

---
+++ webpack.config.js
@@ -0,0 +1,31 @@
+const path = require('path');
+
+const HtmlWebPackPlugin = require("html-webpack-plugin");
+
+module.exports = {    
+    context: path.join(__dirname, 'src'),
+    entry: './index.js',
+    output: {
+        path: __dirname + '/dist',
+        publicPath: '/',
+        filename: 'bundle.js'
+    },
+    module: {
+        rules: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            use: {
+                loader: "babel-loader"
+            }
+        }]
+    },
+    resolve: {
+        modules: [path.resolve(__dirname, 'src'), 'node_modules'],
+        extensions: ['.js', '.jsx']
+    },
+    plugins: [
+        new HtmlWebPackPlugin({
+        template: "./index.html",
+        filename: "index.html"
+    })]
+};
